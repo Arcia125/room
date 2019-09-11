4e6aa300a161ce8c49717ec7d36336d79d24b8c6
Kevin Hallett <kevinmhallett@gmail.com>
2019-09-11 16:15:42 -0500

    serve frontend from server app

--- server/src/app.js
+++ server/src/app.js
@@ -2,15 +2,18 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
 import { schema } from './data/schema';
+import { builtFrontendPath, indexHtmlPath } from './filePaths';
 
 const server = new ApolloServer(schema);
 
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('hello')
-});
-
 server.applyMiddleware({ app });
+
+app.use(express.static(builtFrontendPath));
+
+app.get('/', function (req, res) {
+  res.sendFile(indexHtmlPath);
+});
 
 export { app };
---
+++ server/src/filePaths.js
@@ -0,0 +1,10 @@
+import path from 'path';
+
+const builtFrontendPath =  path.join(process.cwd(), 'build');
+
+const indexHtmlPath = path.join(builtFrontendPath, 'index.html');
+
+export {
+  builtFrontendPath,
+  indexHtmlPath
+};
