3e0d99b7cec930712348b3f141d1af81bb0b9ede
Kevin Hallett <kevinmhallett@gmail.com>
2019-09-20 12:49:45 -0500

    implement basic structure of ClaimAccountForm

--- src/components/ClaimAccountForm.js
+++ src/components/ClaimAccountForm.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ClaimAccountForm = props => {
-  return <form></form>;
+import { useField } from '../hooks/useField';
+
+const ClaimAccountForm = ({}) => {
+  const emailField = useField();
+  const passwordField = useField();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" {...emailField} />
+      <input type="password" {...passwordField} />
+    </form>
+  );
 };
 
 ClaimAccountForm.propTypes = {};
