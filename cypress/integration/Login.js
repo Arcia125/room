6f38424f9e98ad474a43fb1016d5cacdc68b999f
dontehightower <dontedhightower@gmail.com>
2019-09-21 23:16:04 -0500

    webdriver working, test lacks assertions

--- cypress/integration/Login.js
+++ cypress/integration/Login.js
@@ -1,1 +1,9 @@
-cy.visit("http://localhost:3000");
+describe("login", () => {
+  it("allows user to type a username", () => {
+    cy.visit("http://localhost:3000")
+      .get("input")
+      .type("banana")
+      .get(".sc-bwzfXH")
+      .click();
+  });
+});
