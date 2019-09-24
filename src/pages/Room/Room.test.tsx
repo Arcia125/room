a1a1582a3359de68101f157d75053565e66a127f
Kevin Hallet <kevinmhallett@gmail.com>
2019-09-24 13:22:04 -0500

    add Room snapshot test

--- src/pages/Room/Room.test.tsx
+++ src/pages/Room/Room.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, wait } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Room from '.';
@@ -20,8 +20,8 @@ const mocks = [getRoomMock, newRoomMessageMock, newRoomUserMock, joinRoomMock];
 afterEach(cleanup);
 
 describe('Room', () => {
-  it('renders without crashing', async () => {
-    const { container } = render(
+  it('renders without crashing', () => {
+    const { container, asFragment } = render(
       <MockedProvider
         addTypename
         mocks={mocks}
@@ -39,5 +39,32 @@ describe('Room', () => {
       </MockedProvider>
     );
     expect(container).toBeInTheDocument();
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('finishes loading initial queries', async () => {
+    const { getByText, asFragment } = render(
+      <MockedProvider
+        addTypename
+        mocks={mocks}
+        resolvers={currentUserMockResolvers}
+      >
+        <Provider>
+          <Room
+            {...mockRouteComponentProps}
+            match={{
+              ...mockRouteComponentProps.match,
+              params: { roomId: mockRoom.id }
+            }}
+          />
+        </Provider>
+      </MockedProvider>
+    );
+
+    // wait for the room name to be in the document
+    await wait(() => expect(getByText(mockRoom.name)).toBeInTheDocument());
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
