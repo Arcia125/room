c27d93cc5272f643ebce1b3d91434c7199004f24
Kevin Hallet <kevinmhallett@gmail.com>
2019-09-24 12:45:40 -0500

    fix UserList test

--- src/components/UserList.test.tsx
+++ src/components/UserList.test.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { MockedProvider } from '@apollo/react-testing';
-import { render, wait, cleanup } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import UserList from './UserList';
 import { Provider } from '../theme';
 import { users } from '../../shared/mockData/users';
-
-beforeEach(() => {
-  jest.resetAllMocks();
-});
 
 afterEach(cleanup);
 
