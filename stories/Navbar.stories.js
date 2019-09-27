8cab44effaa6b8d61d753f6ba2a714168cf762be
Kevin Hallet <kevinmhallett@gmail.com>
2019-09-27 14:56:12 -0500

    fix storybook with typescript

--- stories/Navbar.stories.js
+++ stories/Navbar.stories.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import Navbar from '../src/components/Navbar';
+import { themeProvider, routerProvider } from './decorators';
 
 export default {
   title: 'Navbar',
   component: Navbar,
+  decorators: [themeProvider, routerProvider],
 };
 
 export const homepage = () => <Navbar />;
